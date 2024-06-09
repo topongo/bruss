3064688ed944faf908e8aa7ecebe17a4e5e61882
topongo <lorenzo.bodini.private@gmail.com>
2024-06-09 17:32:36 +0200

    update to startup script for using cargo-watch

--- startup.sh
+++ startup.sh
@@ -7,6 +7,10 @@ if ! systemctl is-active docker; then
   sudo systemctl start docker
 fi
 docker compose -f app/db/docker-compose.yml up -d
-cargo run -p bruss_api
 
+if ! type cargo-watch > /dev/null; then 
+  cargo install cargo-watch
+fi
+
+cargo watch -x 'run -p bruss_api'
 
